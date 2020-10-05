c5d953ac79b7e997c4748c0fd90c1ff9cf2e17f7
younj1601 <younj1601@wrdsb.ca>
2020-10-05 13:57:14 +0000

    Finished the final while loops task

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *List all of the factors of a positive interger
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //declare interger
+    int num;
+    //declare and initialize counter
+    int counter = 0;
+
+    //force the integer to be positive
+    do{
+      //get the interger from the user
+      System.out.println("Please enter a positive integer to determine its factors:");
+      num = input.nextInt();
+    }while(num <= 0);
+    
+    //give info to user
+    System.out.println("The factors of " + num + " are:");
+
+    //calculate the factors
+    while(counter < num){
+
+      //add one to the counter
+      counter = counter + 1;
+
+      //if num can be divided by counter then it is a factor so it prints it
+      if(num % counter == 0){
+        System.out.println(counter);
+      }
+    }
+
     
   }
 }
